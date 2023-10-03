aa68c7144adf774ff5f3b6ef6a9af376e81c5fa0
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-03 21:00:10 +0500

    header navbar commit

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,5 +1,6 @@
 import helogo from './img/logo.svg'
 import heReklama from './img/header-reklama-img.jpg'
+import { SearchOutlined } from '@ant-design/icons'
 import './header.css'
 
 
@@ -18,6 +19,87 @@ export const Header = () =>{
                 height:'85px',
             }} src={heReklama} alt="Header-reklama-img" />
         </div>
+
+       <div className='header-box'>
+       <nav className='header-nav'>
+            <ul className='header-nav__item'> 
+                <li className='header-nav__item--list'>
+                    <select className='header-nav__item--list-select' name="" id=""> 
+                    <option value="mahalliy" >Mahalliy</option>
+                    <option value="mehridaryo">Mehridaryo</option>
+                    <option value="boshqalar">Boshqalar</option>
+
+
+                    </select>
+                    </li >
+                <li className='header-nav__item--list'>
+                    <select className='header-nav__item--list-select' name="" id="">
+                    <option value="">Markaziy Osiyo</option>
+                        <option value="">Qirg'iston</option>
+                        <option value="">Qozog'iston</option>
+                        <option value="">Turkmaniston</option>
+                        <option value="">Tojikiston</option>
+                        <option value="">Afg'oniston</option>
+                        
+
+                    </select>
+                     </li>
+                <li className='header-nav__item--list'>Dunyo</li>
+                <li className='header-nav__item--list'>
+                    <select className='header-nav__item--list-select' name="" id="">
+                        <option value="">Pul</option>
+                        <option value="">Iqtisodiyot</option>
+                        <option value="">Moliya</option>
+                        <option value="">Kripto</option>
+                        <option value="">Biznes</option>
+                        
+
+                    </select>
+                </li >
+                <li className='header-nav__item--list'>
+                    <select className='header-nav__item--list-select' name="" id="">
+                        <option value="">Madaniyat</option>
+                        <option value="">Kino </option>
+                        <option value="">Kitob</option>
+                        <option value="">Musiqa</option>
+                        <option value="">Shou-biznes</option>
+
+                    </select>
+                </li>
+                <li className="header-nav__item--list">
+                    <select className='header-nav__item--list-select' name="" id=""> 
+                    <option value="">Layfstal</option>
+                    <option value="">Ayollar sahifasi</option>
+                    <option value="">Texnologiyalar</option>
+                    <option value="">Avto</option>
+                    <option value="">Avto</option>
+                    <option value="">Avto</option>
+                    <option value="">Avto</option>
+                    <option value="">Avto</option>
+
+
+                    </select>
+                </li>
+                <li className="header-nav__item--list">Sport</li>
+                <li className="header-nav__item--list">Kolumnistlar</li>
+                <li className="header-nav__item--list">Multimedia</li>
+            </ul>
+       
+
+            </nav>
+        <div className='header-box__card'>
+            <button style={{
+                backgroundColor:"#000",
+                color:'#fff',
+                border:'none'
+            }}>dark</button>
+            <SearchOutlined />
+            <p>UZB</p>
+            <p>==</p>
+        </div>
+      
+       </div>
+       
         </div>
     )
 }
