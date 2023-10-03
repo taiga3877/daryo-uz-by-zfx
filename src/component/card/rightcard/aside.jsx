7e64fdd7907cb98bbd51338c025ddfd25e04b12c
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-04 01:10:10 +0500

    aside tavsif commit

--- src/component/card/rightcard/aside.jsx
+++ src/component/card/rightcard/aside.jsx
@@ -1,4 +1,7 @@
 import './aside.css'
+import Tavsif from './tavsif'
+
+
 import card1Img from './img/card1-img.jpg'
 import card1Img1 from './img/card1-img1.jpg'
 import card1Img2 from './img/card1-img2.jpg'
@@ -9,18 +12,6 @@ import card1Img6 from './img/card1-img6.jpg'
 import card1Img7 from './img/card1-img7.jpg'
 import card1Img8 from './img/card1-img8.jpg'
 import card1Img9 from './img/card1-img9.jpg'
-
-import card2Img from './img/card2-img.jpg'
-import card2Img1 from './img/card2-img1.jpg'
-import card2Img2 from './img/card2-img2.jpg'
-import card2Img3 from './img/card2-img3.jpg'
-
-
-
-
-
-
-
 
 export const AsideCard = () => {
     return (
@@ -178,77 +169,10 @@ export const AsideCard = () => {
                         </div>
                     </div>
                 </div>
-
-                <div className="aside-card__tavsif">
-                    <h3 className='aside-card--heading'>Tavsiya etamiz</h3>
-
+                <div>
+                    <Tavsif />
                 </div>
-                <p className='text--span'></p>
-                <p className='aside-card--text'> </p>
-
-                <div className="aside-box1">
-                    <div className="aside-box1--card1">
-                        <div>
-                            <img className='aside-box--card1__img' src={card2Img} alt="card-img" />
-                        </div>
-                        <div>
-                            <h3 className='aside-box1--card1__heading'>
-                                “Oyiga 80-100 mln so‘mlik dorilar sotilib, o‘zaro taqsimlab kelingan”. “Dok-1 Maks” sudida...
-                            </h3>
-
-                            <p className='aside-box1--card1__text'>
-                                17:00 / 03.10.2023
-                            </p>
-                        </div>
-
-                    </div>
-                    <div className="aside-box1--card1">
-                        <div>
-                            <img className='aside-box--card1__img' src={card2Img1} alt="card-img" />
-                        </div>
-                        <div>
-                            <h3 className='aside-box1--card1__heading'>
-
-                                Farg‘onada tuman bandlikka ko‘maklashish bo‘limi boshlig‘i YTHga uchrab, shifoxonaga...
-                            </h3>
-
-                            <p className='aside-box1--card1__text'>
-                            13:00 / 03.10.2023
-                            </p>
-                        </div>
-
-                    </div>
-                    <div className="aside-box1--card1">
-                        <div>
-                            <img className='aside-box--card1__img' src={card2Img2} alt="card-img" />
-                        </div>
-                        <div>
-                            <h3 className='aside-box1--card1__heading'>
-                            Firibgarlikda ayblanayotgan Tramp, drenajdan chiqqan o‘lik delfinlar, bozordagi ulkan sakkizoyoq — 3-...
-                            </h3>
-
-                            <p className='aside-box1--card1__text'>
-                            23:00 / 03.10.2023
-                            </p>
-                        </div>
-
-                    </div>
-                    <div className="aside-box1--card1">
-                        <div>
-                            <img className='aside-box--card1__img' src={card2Img3} alt="card-img" />
-                        </div>
-                        <div>
-                            <h3 className='aside-box1--card1__heading'>
-                            AQSHning O‘zbekistondagi elchixonasi Green Card’da ishtirok etmoqchi bo‘lganlarga 10 muhim
-                            </h3>
-
-                            <p className='aside-box1--card1__text'>
-                            11:00 / 02.10.2023
-                            </p>
-                        </div>
-
-                    </div>
-                </div>
+                
             </div>
         </div>
     )
