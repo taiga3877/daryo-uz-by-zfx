d34138ed5ca97388b5df6c07d17ec0bf6d62e133
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-04 01:01:24 +0500

    aside css tavsif commit

--- src/component/card/rightcard/aside.jsx
+++ src/component/card/rightcard/aside.jsx
@@ -10,175 +10,246 @@ import card1Img7 from './img/card1-img7.jpg'
 import card1Img8 from './img/card1-img8.jpg'
 import card1Img9 from './img/card1-img9.jpg'
 
+import card2Img from './img/card2-img.jpg'
+import card2Img1 from './img/card2-img1.jpg'
+import card2Img2 from './img/card2-img2.jpg'
+import card2Img3 from './img/card2-img3.jpg'
 
 
 
 
 
-export const AsideCard = () =>{
-    return(
+
+
+
+export const AsideCard = () => {
+    return (
         <div className="container">
-    <div className="aside">
+            <div className="aside">
 
-    
-        <div className="aside-card">
-            <h3 className='aside-card--heading'>So'nggi yangiliklarga o'tish</h3>
-           
-        </div>
-        <p className='text--span'></p>
-        <p className='aside-card--text'> </p>
 
-        <div className='aside-box'>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-            Nobel mukofoti tarixi, kimlarga va qancha pul mukofoti beriladi?
-            </h3>
+                <div className="aside-card">
+                    <h3 className='aside-card--heading'>So'nggi yangiliklarga o'tish</h3>
 
-            <p className='aside-box--card1__text'>
-            BUGUN, 20:35
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Britaniya hukumati xorijdagi qamoqxonalarni ijaraga olishga qaror qildi
-            </h3>
+                </div>
+                <p className='text--span'></p>
+                <p className='aside-card--text'> </p>
 
-            <p className='aside-box--card1__text'>
-            KECHA, 21:50
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img5} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Prezidentning xorijiy tashriflaridan asosiy maqsad nimalar ekani aytildi
-            </h3>
+                <div className='aside-box'>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Nobel mukofoti tarixi, kimlarga va qancha pul mukofoti beriladi?
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            BUGUN, 20:35
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img2} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          OCHL. “Al-Fayha” “Paxtakor” ustidan ishonchli hisobda g‘alaba qozondi
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                BUGUN, 20:35
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Britaniya hukumati xorijdagi qamoqxonalarni ijaraga olishga qaror qildi
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            KECHA, 22:10
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img6} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Instagram: Dilshodbek Kattabekov uylandi, Munisa Rizayeva onasining tug‘ilgan...
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                KECHA, 21:50
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img5} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Prezidentning xorijiy tashriflaridan asosiy maqsad nimalar ekani aytildi
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            BUGUN, 20:35
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img4} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Qozog‘istonda ko‘mir eksportiga qo‘yilgan taqiq uzaytirildi
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                BUGUN, 20:35
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img2} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                OCHL. “Al-Fayha” “Paxtakor” ustidan ishonchli hisobda g‘alaba qozondi
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            BUGUN, 20:35
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img7} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Tan olinmagan Tog‘li Qorabog‘ Respublikasi sobiq prezidentlari hibsga olindi
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                KECHA, 22:10
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img6} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Instagram: Dilshodbek Kattabekov uylandi, Munisa Rizayeva onasining tug‘ilgan...
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            KECHA, 22:25
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img1} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Rossiyada urushdan qaytgan “vagnerchi” uyga o‘t qo‘yib, ayollarni yoqib yubordi
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                BUGUN, 20:35
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img4} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Qozog‘istonda ko‘mir eksportiga qo‘yilgan taqiq uzaytirildi
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            KECHA, 22:10
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img3} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Toshkentda Qatar amiri taʼsis etgan korrupsiyaga qarshi kurashda erishilgan yutuqlar...
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                BUGUN, 20:35
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img7} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Tan olinmagan Tog‘li Qorabog‘ Respublikasi sobiq prezidentlari hibsga olindi
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            KECHA, 23:10
+                            <p className='aside-box--card1__text'>
+                                KECHA, 22:25
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img1} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Rossiyada urushdan qaytgan “vagnerchi” uyga o‘t qo‘yib, ayollarni yoqib yubordi
+                            </h3>
 
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img8} alt="card-img" />
-          </div>
-        </div>
-        <div className="aside-box--card1">
-          <div>
-          <h3 className='aside-box--card1__heading'>
-          Firibgarlikda ayblanayotgan Tramp, drenajdan chiqqan o‘lik delfinlar, bozordagi ulkan
-            </h3>
+                            <p className='aside-box--card1__text'>
+                                KECHA, 22:10
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img3} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Toshkentda Qatar amiri taʼsis etgan korrupsiyaga qarshi kurashda erishilgan yutuqlar...
+                            </h3>
 
-            <p className='aside-box--card1__text'>
-            BUGUN, 20:15
-            </p>
-          </div>
-          <div>
-            <img  className='aside-box--card1__img' src={card1Img9} alt="card-img" />
-          </div>
-        </div>
-        </div>
+                            <p className='aside-box--card1__text'>
+                                KECHA, 23:10
 
-        <div className="aside-card__tavsif">
-            <h3 className='aside-card--heading'>Tavsiya etamiz</h3>
-           
-        </div>
-        <p className='text--span'></p>
-        <p className='aside-card--text'> </p>
-        </div>
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img8} alt="card-img" />
+                        </div>
+                    </div>
+                    <div className="aside-box--card1">
+                        <div>
+                            <h3 className='aside-box--card1__heading'>
+                                Firibgarlikda ayblanayotgan Tramp, drenajdan chiqqan o‘lik delfinlar, bozordagi ulkan
+                            </h3>
+
+                            <p className='aside-box--card1__text'>
+                                BUGUN, 20:15
+                            </p>
+                        </div>
+                        <div>
+                            <img className='aside-box--card1__img' src={card1Img9} alt="card-img" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="aside-card__tavsif">
+                    <h3 className='aside-card--heading'>Tavsiya etamiz</h3>
+
+                </div>
+                <p className='text--span'></p>
+                <p className='aside-card--text'> </p>
+
+                <div className="aside-box1">
+                    <div className="aside-box1--card1">
+                        <div>
+                            <img className='aside-box--card1__img' src={card2Img} alt="card-img" />
+                        </div>
+                        <div>
+                            <h3 className='aside-box1--card1__heading'>
+                                “Oyiga 80-100 mln so‘mlik dorilar sotilib, o‘zaro taqsimlab kelingan”. “Dok-1 Maks” sudida...
+                            </h3>
+
+                            <p className='aside-box1--card1__text'>
+                                17:00 / 03.10.2023
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="aside-box1--card1">
+                        <div>
+                            <img className='aside-box--card1__img' src={card2Img1} alt="card-img" />
+                        </div>
+                        <div>
+                            <h3 className='aside-box1--card1__heading'>
+
+                                Farg‘onada tuman bandlikka ko‘maklashish bo‘limi boshlig‘i YTHga uchrab, shifoxonaga...
+                            </h3>
+
+                            <p className='aside-box1--card1__text'>
+                            13:00 / 03.10.2023
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="aside-box1--card1">
+                        <div>
+                            <img className='aside-box--card1__img' src={card2Img2} alt="card-img" />
+                        </div>
+                        <div>
+                            <h3 className='aside-box1--card1__heading'>
+                            Firibgarlikda ayblanayotgan Tramp, drenajdan chiqqan o‘lik delfinlar, bozordagi ulkan sakkizoyoq — 3-...
+                            </h3>
+
+                            <p className='aside-box1--card1__text'>
+                            23:00 / 03.10.2023
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="aside-box1--card1">
+                        <div>
+                            <img className='aside-box--card1__img' src={card2Img3} alt="card-img" />
+                        </div>
+                        <div>
+                            <h3 className='aside-box1--card1__heading'>
+                            AQSHning O‘zbekistondagi elchixonasi Green Card’da ishtirok etmoqchi bo‘lganlarga 10 muhim
+                            </h3>
+
+                            <p className='aside-box1--card1__text'>
+                            11:00 / 02.10.2023
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
