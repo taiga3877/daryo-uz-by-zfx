db737181d3e4520e8b088baa499b8532a8f5ad2b
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-03 22:50:25 +0500

    index html head commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Header } from "./component/header"
+import { BrowserRouter, Route,  Routes } from "react-router-dom"
+import { Footer } from "antd/es/layout/layout"
 
 
 function App() {
   return(
-    <>
+    <BrowserRouter>
     <Header/>
-    </>
+    <div>
+      <div className="right">
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </div>
+      <div className="left"></div>
+    </div>
+    <Footer />
+    <Routes>
+      <Route path="/" />
+    </Routes>
+
+    </BrowserRouter>
   )
  
 }
