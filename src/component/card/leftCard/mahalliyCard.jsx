b2018f40203433f5c3c57246a1eea5a8bce50d92
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-04 22:32:36 +0500

    card mahaliyCard css commit

--- src/component/card/leftCard/mahalliyCard.jsx
+++ src/component/card/leftCard/mahalliyCard.jsx
@@ -1,18 +1,20 @@
 import './leftMain.css'
 
 
-import rightImg from './img/right-img.jpg'
-import rightImg1 from './img/right-img1.jpg'
-import rightImg2 from './img/right-img2.jpg'
-import rightImg3 from './img/right-img3.jpg'
+import mahhalliyImg from './img/mahalliy-img.jpg'
+import mahhalliyImg1 from './img/mahalliy-img1.jpg'
+import mahhalliyImg2 from './img/mahalliy-img2.jpg'
+import mahhalliyImg3 from './img/mahalliy-img3.jpg'
 
 
-function MahalliyCard (){
-    return(
+
+
+function MahalliyCard() {
+    return (
         <div>
 
 
-<div className="aside-card__dunyo">
+            <div className="aside-card__dunyo">
                 <h3 className='aside-card__dunyo--heading'>Mahalliy </h3>
 
             </div>
@@ -20,90 +22,97 @@ function MahalliyCard (){
             <p className='aside-card__dunyo--text'> </p>
 
 
-            
+
 
             <div className="card-leftDunyo">
+
+
+                <div className='card-leftDunyo--right'>
+                    <div className="card-leftDunyo--right__card">
+                        <img className='card-leftDunyo--right__card--img' src={mahhalliyImg} alt="img" />
+                        <div>
+                            <h4 className="card-leftDunyo--right__card--heading">
+                                O‘zbekistonning 9 ta viloyatida sel-suv toshqini xavfi eʼlon qilindi
+                            </h4>
+
+                            <p className="card-leftDunyo--right__card--text">
+                                15:00 / 04.10.2023
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card-leftDunyo--right__card">
+                        <img className='card-leftDunyo--right__card--img' src={mahhalliyImg1} alt="img" />
+                        <div>
+                            <h4 className="card-leftDunyo--right__card--heading">
+                                Shavkat Mirziyoyev Putin taklifiga binoan 5-oktyabr kuni Rossiyaga boradi
+                            </h4>
+
+                            <p className="card-leftDunyo--right__card--text">
+                                15:00 / 04.10.2023
+
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card-leftDunyo--right__card">
+                        <img className='card-leftDunyo--right__card--img' src={mahhalliyImg2} alt="img" />
+                        <div>
+                            <h4 className="card-leftDunyo--right__card--heading">
+
+                                “Ular qahramon”— mutaxassis 160 kilometrlik tirbandlikda qolib ketgan o‘zbek haydovchilari haqida
+
+                            </h4>
+
+                            <p className="card-leftDunyo--right__card--text">
+                                13:00 / 04.10.2023
+
+
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card-leftDunyo--right__card">
+                        <img className='card-leftDunyo--right__card--img' src={mahhalliyImg3} alt="img" />
+                        <div>
+                            <h4 className="card-leftDunyo--right__card--heading">
+
+                                Kanada elchisi O‘zbekistondagi diplomatik missiyasini yakunlamoqda
+                            </h4>
+
+                            <p className="card-leftDunyo--right__card--text">
+                                13:00 / 04.10.2023
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
                 <div>
 
-               
-                <div className="card-leftDunyo--left">
-                    <p className="card-leftDunyo--left__text">
-                        DUNYO 
-                    </p>
 
-                    
-                </div>
-                <h4 className="card-leftDunyo--left__heading">
-                    Kimyo bo‘yicha Nobel mukofoti uch nafar olimga berildi, ulardan biri — asli rossiyalik
+                    <div className="card-leftDunyo--left--mahalliy">
+                        <p className="card-leftDunyo--left__text">
+                            MAHALLIY
+                        </p>
+
+
+                    </div>
+                    <h4 className="card-leftDunyo--left__heading">
+                        O‘zbekistonda dollar kursi ikki kunlik ko‘tarilishdan so‘ng pastladi
                     </h4>
 
                     <p className='card-leftDunyo--left__text1'>
-                    15:00 / 04.10.2023
+                        16:00 / 04.10.2023
+
                     </p>
 
 
                     <p className='card-leftDunyo--left__dec'>
-                    Mukofot olimlarga “kvant nuqtalarini yaratish va sintez qilish usulini ishlab chiqqani uchun&r...
+                        Markaziy bank xorijiy valyutalarning 5-oktyabrda amalda bo‘ladigan qiymatini e’lon qildi...
+
                     </p>
                 </div>
 
-                <div className='card-leftDunyo--right'>
-                    <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg} alt="img" />
-                       <div>
-                       <h4 className="card-leftDunyo--right__card--heading">
-                       Urushga qarshi chiqqan rossiyalik jurnalist Marina Ovsyannikova 8,5 yilga koloniyaga hukm qilindi 
-                        </h4>
-
-                        <p className="card-leftDunyo--right__card--text">
-                        14:00 / 04.10.2023       
-                        </p>
-                       </div>
-                    </div>
-                    <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg1} alt="img" />
-                       <div>
-                       <h4 className="card-leftDunyo--right__card--heading">
-                       Turkiya IIV Anqarada terakt uyushtirganlarning shaxsini aniqladi
-                        </h4>
-
-                        <p className="card-leftDunyo--right__card--text">
-                        13:00 / 04.10.2023
-      
-                        </p>
-                       </div>
-                    </div>
-                    <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg2} alt="img" />
-                       <div>
-                       <h4 className="card-leftDunyo--right__card--heading">
-                       Rossiya osmonida tunda 31 ta Ukraina droni urib tushirilgani aytildi
-                        </h4>
-
-                        <p className="card-leftDunyo--right__card--text">
-                        13:00 / 04.10.2023
-     
-                        </p>
-                       </div>
-                    </div>
-                    <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg3} alt="img" />
-                       <div>
-                       <h4 className="card-leftDunyo--right__card--heading">
-                       Ukrainaning Yevropa Ittifoqiga qo‘shilishi bo‘yicha muzokaralar yil oxirigacha boshlanadi — Politico
-                        </h4>
-
-                        <p className="card-leftDunyo--right__card--text">
-                        11:00 / 04.10.2023       
-                        </p>
-                       </div>
-                    </div>
-                </div>
-
-
-               
             </div>
-            <button className="card-leftDunyo--btn"> <b>DUNYO</b>-ENG SO'NGGI XABARLAR </button>
+            <button className="card-leftDunyo--btn"> <b>MAHALLIY</b>-ENG SO'NGGI XABARLAR </button>
 
         </div>
     )
