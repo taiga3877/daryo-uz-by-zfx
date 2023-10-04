8904fa4bca5856ce8331955487c600de9cf54eda
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-05 00:11:09 +0500

    card img commit

--- src/component/card/leftCard/lifestyleCard.jsx
+++ src/component/card/leftCard/lifestyleCard.jsx
@@ -1,10 +1,11 @@
 import './leftMain.css'
 
-
-import rightImg from './img/right-img.jpg'
-import rightImg1 from './img/right-img1.jpg'
-import rightImg2 from './img/right-img2.jpg'
-import rightImg3 from './img/right-img3.jpg'
+import lyfImg from './img/lyf-img.jpg'
+import lyfImg1 from './img/lyf-img1.jpg'
+import lyfImg2 from './img/lyf-img2.jpg'
+import lyfImg3 from './img/lyf-img3.jpg'
+import lyfImg4 from './img/lyf-img4.jpg'
+import lyfImg5 from './img/lyf-img5.jpg'
 
 
 function LifestyleCard() {
@@ -51,7 +52,7 @@ function LifestyleCard() {
                 <div className="aaaaaaaaaa">
                 <div className='card-leftDunyo--right--lyf'>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                        Mashhurlar merosi: Jahon yulduzlaridan kimlar boyligini farzandlariga qoldirmoqchi emas? 
@@ -64,7 +65,7 @@ function LifestyleCard() {
                        </div>
                     </div>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg1} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg1} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                        “Garri Potter” franshizasida eng ko‘p ekran vaqtiga ega bo‘lgan qahramonlar (foto)
@@ -78,7 +79,7 @@ function LifestyleCard() {
                        </div>
                     </div>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg2} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg2} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                        Mashhurlar qanday qilib shartnoma imzolamasdan, taniqli brendlarning millionlab dollar foyda olishiga sababchi bo‘ladi?...
@@ -97,7 +98,7 @@ function LifestyleCard() {
                 <div className="bbbb">
                 <div className='card-leftDunyo--right--lyf '>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg3} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                        Magniy tanqisligi va noto‘g‘ri ovqatlanish: Tez charchab qolishning asosiy sabablari 
@@ -110,7 +111,7 @@ function LifestyleCard() {
                        </div>
                     </div>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg1} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg4} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                        Yurak sanchib og‘riganda: Inson hayotini saqlab qolishi mumkin bo‘lgan maslahatlar  
@@ -124,7 +125,7 @@ function LifestyleCard() {
                        </div>
                     </div>
                     <div className="card-leftDunyo--right__card">
-                        <img className='card-leftDunyo--right__card--img' src={rightImg2} alt="img" />
+                        <img className='card-leftDunyo--right__card--img' src={lyfImg5} alt="img" />
                        <div>
                        <h4 className="card-leftDunyo--right__card--heading">
                         Iordaniya qirolichasi Raniya al-Abdullaning bir xil vaznda yurishining siri nimada?
