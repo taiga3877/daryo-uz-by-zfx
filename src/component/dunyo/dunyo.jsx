33b21784c2b74681b26a213537e54d48efd3c8dd
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-05 10:59:14 +0500

    dunyo css commit

--- src/component/dunyo/dunyo.jsx
+++ src/component/dunyo/dunyo.jsx
@@ -5,6 +5,31 @@ import './dunyo.css'
     return( 
         <div>
             <h2 className='card--dunyo__heading'>DUNYO </h2> 
+
+            <div className="card--dunyo__box">
+              <div>
+              <div className="card--dunyo__box--wrapper">
+                  <p className='card--dunyo__box--wrapper--text'>DUNYO</p>
+              </div>
+              <h3 className='card--dunyo__box--wrapper--heading'>
+              Bayden Ukrainaga ATACMS raketalarini yetkazib berish haqidagi savolga javob berdi
+              </h3>
+              <span className='card--dunyo__box--wrapper--span'>
+            BUGUN, 10:22
+
+              </span>
+              <p className='card--dunyo__box--wrapper--dec'>
+              OAV AQSH prezidenti Ukrainaga kassetali o‘q-dorilar bilan birga ATACMS raketalarini berishga qaror qilgani haqida xabar bergandi
+              </p>
+              </div>
+
+             <div>
+             <div className="card--dunyo__box--wrapper">
+              <p className='card--dunyo__box--wrapper--text'>DUNYO</p>
+                
+                </div>
+             </div>
+            </div>
         </div>
     )
   }
