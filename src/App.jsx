d74547600f9df41ba494e82649304707566fbada
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-10-06 01:33:41 +0500

    error page commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { LeftMain } from "./component/card/leftCard"
 import { Dunyo } from "./component/dunyo"
 import { Mahalliy } from "./component/mahalliy"
 import { Footer } from "./component/footer"
+// import { Error } from "./component/error"
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<LeftMain/>}></Route>
           <Route path="/category/dunyo" element={<Dunyo/>}></Route>
           <Route path="/category/mahalliy" element={<Mahalliy/>}></Route>
+          {/* <Route path="*" element={<Error/>}></Route> */}
 
         </Routes>
       </div>
